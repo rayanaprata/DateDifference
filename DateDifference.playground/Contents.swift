7488a8eb9ff20a07125dc68e6e8f1119a4c6e129
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2022-01-04 16:03:44 -0300

    date difference using extension

--- DateDifference.playground/Contents.swift
+++ DateDifference.playground/Contents.swift
@@ -3,6 +3,33 @@ import UIKit
 let calendar = Calendar.current
 
 let date1 = calendar.date(bySettingHour: 15, minute: 30, second: 00, of: Date())!
-let date2 = calendar.date(bySettingHour: 16, minute: 00, second: 00, of: Date())!
+let date2 = calendar.date(bySettingHour: 17, minute: 15, second: 43, of: Date())!
 
 let result = date2.timeIntervalSince(date1)
+
+let time = Int(result)
+
+let seconds = time % 60
+let minutes = (time / 60) % 60
+let hours = time / 3600
+
+struct DateHMS {
+    let second: Int
+    let minute: Int
+    let hour: Int
+}
+
+extension Date {
+    func diffToHMS(by date: Date) -> DateHMS {
+        let result = date2.timeIntervalSince(date1)
+        let time = Int(result)
+        let seconds = time % 60
+        let minutes = (time / 60) % 60
+        let hours = time / 3600
+        return DateHMS(second: seconds,
+                       minute: minutes,
+                       hour: hours)
+    }
+}
+
+print(date1.diffToHMS(by: date2))
