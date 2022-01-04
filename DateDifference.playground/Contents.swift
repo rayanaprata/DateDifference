3728a6a33111a64eb33b05f768e81ade1fabe9cc
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2022-01-04 15:56:12 -0300

    calculate date time interval

--- DateDifference.playground/Contents.swift
+++ DateDifference.playground/Contents.swift
@@ -1,2 +1,8 @@
 import UIKit
 
+let calendar = Calendar.current
+
+let date1 = calendar.date(bySettingHour: 15, minute: 30, second: 00, of: Date())!
+let date2 = calendar.date(bySettingHour: 16, minute: 00, second: 00, of: Date())!
+
+let result = date2.timeIntervalSince(date1)
